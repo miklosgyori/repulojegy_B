82aed5543518b852f2715f8a8d78caa94c60a5d5
Miklos Gyori <61111659+miklosgyori@users.noreply.github.com>
2024-10-28 23:16:59 +0100

    ADDING class JegyFoglalas

---
+++ jegyfoglalas.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from jarat import Jarat
+
+
+class JegyFoglalas:
+    """
+    A járatok foglalásához szükséges osztály, amely egy utazásra szóló jegy foglalását tárolja.
+
+    Attributumok:
+        szam (int): foglalas szama
+        nev (str): utas neve
+        jarat (Jarat): jarat
+        indulas (datetime): indulasi datum es ido
+    """
+
+    def __init__(self, szam: int, nev: str, jarat: Jarat, indulas: datetime):
+        self.szam = szam
+        self.nev = nev
+        self.jarat = jarat
+        self.indulas = indulas
