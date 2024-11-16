f8ba3934eca92d9d03e2970541a49074b13a5c61
Miklos Gyori <61111659+miklosgyori@users.noreply.github.com>
2024-11-16 23:28:54 +0100

    IMPROVEMENTS
    Improved functionalities in: cli.py, main.py

--- cli.py
+++ cli.py
@@ -1,10 +1,13 @@
 import sys
 
+from foglalasirendszer import FoglalasiRendszer
 
-def mutat_menu():
+
+def mutat_menu(foglalasi_rendszer: FoglalasiRendszer):
     """
     A menut irja ki.
     """
+    print("\n")
     print("-" * 30)
     print("MENU")
     print("1) Jegy foglalasa: 1")
@@ -12,35 +15,43 @@ def mutat_menu():
     print("3) Foglalasok listazasa: 3")
     print("X) Kilepes a programbol: X")
     print("-" * 30)
-    valasz = input(" Nyomd meg a valasztott billentyut!")
+    valasz = input("Nyomd meg a valasztott billentyut, majd az ENTER-t!")
     if valasz == "1":
-        foglal_jegyet()
+        foglal_jegyet(foglalasi_rendszer)
     elif valasz == "2":
-        lemond_foglalast()
+        lemond_foglalast(foglalasi_rendszer)
     elif valasz == "3":
-        listaz_foglalast()
+        listaz_foglalast(foglalasi_rendszer)
     elif valasz == "X":
         kilep()
     else:
         print("Ervenytelen valasztas. Valassz ervenyes lehetoseget")
 
 
-def foglal_jegyet():
-    print("Jegyfoglalas:")
+def foglal_jegyet(foglalasi_rendszer: FoglalasiRendszer):
+    print("\n*** Jegyfoglalas: ***")
+    print("Listazom a jaratokat, legitarsasagok szerint:")
+    for legitarsasag in foglalasi_rendszer.legitarsasagok:
+        print(legitarsasag)
     # TODO logika!!!
 
 
-def lemond_foglalast():
-    print("Foglalas lemondasa:")
+def lemond_foglalast(foglalasi_rendszer: FoglalasiRendszer):
+    print("\n*** Foglalas lemondasa: ***")
+    print("Az ervenyes foglalasok:")
+    for foglalas in foglalasi_rendszer.foglalasok:
+        print(foglalas)
     # TODO logika!!!
 
 
-def listaz_foglalast():
-    print("Foglalasok listazasa:")
-    # TODO logika!!!
+def listaz_foglalast(foglalasi_rendszer: FoglalasiRendszer):
+    print("\n*** Foglalasok listazasa: ***\n")
+    for foglalas in foglalasi_rendszer.foglalasok:
+        print(foglalas)
 
 
 def kilep():
+    print("\n*** Kilepes: ***")
     valasz = input("Biztosan ki akarsz lépni? Akkor nyomd meg az \"I\" billentyut!")
     if valasz == "I":
         print("Kilepek.")
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import utils
+from cli import mutat_menu
 from foglalasirendszer import FoglalasiRendszer
 
 if __name__ == '__main__':
     foglalasi_rendszer = FoglalasiRendszer()
     utils.inicializal(foglalasi_rendszer)
+    while True:
+        mutat_menu(foglalasi_rendszer)
